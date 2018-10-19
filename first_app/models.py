afc872152a5899a6f4ce9eefc0f08687b5516cfa
Kudzai Nyandoro <kudzain@gmail.com>
2018-10-19 05:00:53 -0700

    Add models and run migrations.

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264,unique=True)
+
+def __str__(self):
+    return self.top_name
+
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic)
+    name = models.CharField(max_length=264,unique=True)
+    url = models.URLField(unique=True)
+
+def __str__(self):
+    return self.name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
+
+# $ python manage.py migrate
+# $ python manage.py makemigrations first_app
+# $ python manage.py migrate
+# $ python manage.py shell
+# >>> from first_app.models import Topic
+# >>> print(Topic.objects.all())
+# >>> t = Topic(top_name="Social Network")
+# >>> t.save()
+# >>> print(Topic.objects.all())
